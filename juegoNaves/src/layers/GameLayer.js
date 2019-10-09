325fa626aafa3165b141efc1cf940e52e3203536
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-10-09 06:29:28 +0200

    player can shoot but not to death

--- juegoNaves/src/layers/GameLayer.js
+++ juegoNaves/src/layers/GameLayer.js
@@ -3,63 +3,78 @@ class GameLayer extends Layer {
         super();
         this.iniciar();
     }
+
     iniciar() {
         this.jugador = new Jugador(50, 50);
-        this.fondo = new Fondo(imagenes.fondo,480*0.5,320*0.5);
+        this.fondo = new Fondo(imagenes.fondo, 480 * 0.5, 320 * 0.5);
+
+        this.disparosJugador = []
 
         this.enemigos = [];
-        this.enemigos.push(new Enemigo(300,50));
-        this.enemigos.push(new Enemigo(350,200));
+        this.enemigos.push(new Enemigo(300, 50));
+        this.enemigos.push(new Enemigo(350, 200));
     }
 
-    actualizar (){
+    actualizar() {
         this.jugador.actualizar();
-        for (var i=0; i < this.enemigos.length; i++){
+        for (var i = 0; i < this.enemigos.length; i++) {
             this.enemigos[i].actualizar();
         }
 
+        // disparos
+        for (var i = 0; i < this.disparosJugador.length; i++) {
+            this.disparosJugador[i].actualizar();
+        }
+
         // colisiones
-        for (var i=0; i < this.enemigos.length; i++){
-            if ( this.jugador.colisiona(this.enemigos[i])){
+        for (var i = 0; i < this.enemigos.length; i++) {
+            if (this.jugador.colisiona(this.enemigos[i])) {
                 this.iniciar();
             }
         }
     }
 
-    dibujar (){
+    dibujar() {
         this.fondo.dibujar();
+        for (var i = 0; i < this.disparosJugador.length; i++) {
+            this.disparosJugador[i].dibujar();
+        }
         this.jugador.dibujar();
-        for (var i=0; i < this.enemigos.length; i++){
+        for (var i = 0; i < this.enemigos.length; i++) {
             this.enemigos[i].dibujar();
         }
 
     }
-    procesarControles( ){
+
+    procesarControles() {
         // disparar
-        if ( controles.disparo ){
+        if (controles.disparo) {
+            var nuevoDisparo = this.jugador.disparar();
+            if (nuevoDisparo != null) {
+                this.disparosJugador.push(nuevoDisparo);
+            }
 
         }
         // Eje X
-        if ( controles.moverX > 0 ){
+        if (controles.moverX > 0) {
             this.jugador.moverX(1);
-        }else if ( controles.moverX < 0){
+        } else if (controles.moverX < 0) {
             this.jugador.moverX(-1);
         } else {
             this.jugador.moverX(0);
         }
 
         // Eje Y
-        if ( controles.moverY > 0 ){
+        if (controles.moverY > 0) {
             this.jugador.moverY(-1);
 
-        } else if ( controles.moverY < 0 ){
+        } else if (controles.moverY < 0) {
             this.jugador.moverY(1);
         } else {
             this.jugador.moverY(0);
         }
 
     }
-
 
 
 }
--- juegoNaves/src/modelos/Jugador.js
+++ juegoNaves/src/modelos/Jugador.js
@@ -1,19 +1,40 @@
 class Jugador extends Modelo {
     constructor(x, y) {
-        super(imagenes.jugador , x, y);
+        super(imagenes.jugador, x, y);
         this.vx = 0; // velocidadX
         this.vy = 0; // velocidadY
+
+        // Disparo
+        this.cadenciaDisparo = 30; // 1 segundo
+        this.tiempoDisparo = 0;
     }
 
-    actualizar(){
+    actualizar() {
         this.x = this.x + this.vx;
         this.y = this.y + this.vy;
+        // Tiempo Disparo
+        if ( this.tiempoDisparo > 0 ) {
+            this.tiempoDisparo--;
+        }
     }
 
-    moverX (direccion){
+    moverX(direccion) {
         this.vx = direccion * 3;
     }
-    moverY (direccion){
+
+    moverY(direccion) {
         this.vy = direccion * 3;
     }
+
+    disparar() {
+        if (this.tiempoDisparo == 0) {
+            // reiniciar Cadencia
+            this.tiempoDisparo = this.cadenciaDisparo;
+            return new DisparoJugador(this.x, this.y);
+        } else {
+            return null;
+        }
+
+    }
+
 }
