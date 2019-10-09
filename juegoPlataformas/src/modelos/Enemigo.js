7f8034bdcf7c8c7627b854ab821946b221557ba8
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-10-09 07:13:55 +0200

    beginning of platformssss

---
+++ juegoPlataformas/src/modelos/Enemigo.js
@@ -0,0 +1,11 @@
+class Enemigo extends Modelo {
+    constructor(x, y) {
+        super(imagenes.enemigo, x, y)
+        this.vy = 0;
+        this.vx = 1;
+    }
+    actualizar (){
+        this.vx = -1;
+        this.x = this.x + this.vx;
+    }
+}
