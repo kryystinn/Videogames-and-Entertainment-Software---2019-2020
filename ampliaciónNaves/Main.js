aceebb3cd9e1212229a330b18ee60e244c07244f
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-10-30 02:02:39 +0100

    starting

---
+++ ampliaciónNaves/Main.js
@@ -0,0 +1,44 @@
+// Canvas y contexto del Canvas
+var canvas = document.getElementById("canvas");
+var contexto = canvas.getContext("2d");
+var escaladoMinimo = 1;
+
+// Controles
+var controles = {};
+
+
+// Capas
+var gameLayer;
+
+// Inicio capas y bucle del juego
+function iniciarJuego() {
+    gameLayer = new GameLayer();
+    setInterval(loop, 1000 / 30);
+}
+
+
+
+function loop(){
+    console.log("loop - ")
+    gameLayer.actualizar();
+    gameLayer.procesarControles()
+    gameLayer.dibujar();
+}
+
+
+
+// Cambio de escalado
+window.addEventListener('load', resize, false);
+
+function resize() {
+    console.log("Resize")
+    var escaladoAncho = parseFloat(window.innerWidth / canvas.width);
+    var escaladoAlto = parseFloat(window.innerHeight / canvas.height);
+
+    escaladoMinimo = Math.min(escaladoAncho, escaladoAlto);
+
+    canvas.width = canvas.width*escaladoMinimo;
+    canvas.height = canvas.height*escaladoMinimo;
+
+    contexto.scale(escaladoMinimo,escaladoMinimo);
+}
---
+++ ampliaciónNaves/src/modelos/Enemigo.js
@@ -0,0 +1,26 @@
+class Enemigo extends Modelo {
+
+    constructor(x, y) {
+        super(imagenes.enemigo, x, y)
+        this.aMover = new Animacion(imagenes.enemigo_movimiento,
+            this.ancho, this.alto, 6, 3);
+        // Ref a la animación actual
+        this.animacion = this.aMover;
+
+        this.vy = 0;
+        this.vx = 1;
+    }
+
+    actualizar (){
+        this.animacion.actualizar();
+
+        this.vx = -1;
+        this.x = this.x + this.vx;
+
+    }
+
+    dibujar (){
+        this.animacion.dibujar(this.x, this.y);
+    }
+
+}
